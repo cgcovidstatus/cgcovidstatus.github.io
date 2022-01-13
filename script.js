1bdfda05977156e3659db33afd46b90f3d7bf763
cgcovidstatus <96871524+cgcovidstatus@users.noreply.github.com>
2022-01-13 22:17:36 +0530

    T

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ let revChart = new Chart(ctx, {
       
       
       
-"jan 03",
+
 "jan 04",
 "jan 05",
 "jan 06",
@@ -23,7 +23,8 @@ let revChart = new Chart(ctx, {
 "jan 09",
 "jan 10",
 "jan 11",
-"jan 12"
+"jan 12",
+"jan 13"
      
     ],
     datasets: [
@@ -31,19 +32,19 @@ let revChart = new Chart(ctx, {
         label: "positive",
         borderColor: "red",
         backgroundColor: "rgba(235, 247, 245, 0.5)",
-        data: [698, 1059, 1615, 2400, 2828, 3455, 2502, 4120, 5151, 5476]
+        data: [1059, 1615, 2400, 2828, 3455, 2502, 4120, 5151, 5476, 6051]
       },
       {
         label: "recovered",
         borderColor: "green",
         backgroundColor: "rgba(233, 238, 253, 0.5)",
-        data: [29, 21, 29, 56, 46, 69, 102, 358, 483, 1933]
+        data: [21, 29, 56, 46, 69, 102, 358, 483, 1933, 4636]
       },
 	   {
         label: "death",
         borderColor: "black",
         backgroundColor: "rgba(233, 238, 253, 0.5)",
-        data: [0, 0, 3, 1, 1, 3, 4, 2, 4, 4, 4]
+        data: [0, 3, 1, 1, 3, 4, 2, 4, 4, 4, 7]
       } 
     ]
   },
