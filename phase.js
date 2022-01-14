a4b548bc6a38bf7ab50b54ebf78ca730ed9e4336
cgcovidstatus <96871524+cgcovidstatus@users.noreply.github.com>
2022-01-14 18:48:05 +0530

    Add files via upload

---
+++ phase.js
@@ -0,0 +1,37 @@
+const moviesUrl =
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vTxLUDfldyQ6U4MHEr4DrenRk3lKEJAimGBQxSQrGZ4FOfIuAZzrWQk_SfCyBJ_UCgiZP5mTPc24dco/pub?output=csv";
+
+const showsUrl =
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vTtKbv_S8Fdo3HLhm64Tc94WZ6FuqtzqePIjuejNFJxKkUvAE8JF8V2KgKoz1n5jQUDfL8A3F-QoDWk/pub?gid=1364847678&single=true&output=csv";
+
+const app = new Vue({
+  el: "#app",
+  data: function () {
+    return {
+      movies: [],
+      shows: []
+    };
+  },
+  created: function () {
+    this.fetchMovies();
+    this.fetchShows();
+  },
+  methods: {
+    fetchMovies() {
+      
+      Papa.parse(moviesUrl, {
+        download: true,
+        header: true,
+        complete: (results) => this.movies = results.data
+      });
+    },
+    fetchShows() {
+      const _this = this;
+      Papa.parse(showsUrl, {
+        download: true,
+        header: true,
+        complete: function(results) { _this.shows = results.data; }
+      });
+    }
+  }
+});
